0e950bb22f0a9e0fb3b4ece7e458156dc7c066f5
ShiniTada <yana.bogdanovich2000@mail.ru>
2018-10-22 23:54:17 +0300

    Change _1_6

--- task_1_1/src/_1_6.java
+++ task_1_1/src/_1_6.java
@@ -1,11 +1,10 @@
 public class _1_6 {
     public static void main(String[] args) {
         int num =1234567;
-        int rev, i = 0;
+        int  i = 0;
         int[] mas = new int[10];
         while(num != 0) {
-            rev = num %10;
-            mas[i] = rev;
+            mas[i]  = num %10;
             i++;
             num /= 10;
         }
